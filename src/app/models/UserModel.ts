ea932d40d87cf58f1bf9dc73f359d6967efce7e2
Mark Riaz <markriaz13@gmail.com>
2025-01-18 23:45:14 +0200

    slightly upgrate model

--- src/app/models/UserModel.ts
+++ src/app/models/UserModel.ts
@@ -1,1 +1,15 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { PutCommand, DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+import { type PutCommandInput, type PutCommandOutput } from "@aws-sdk/lib-dynamodb";
+
+export class UserModel {
+    //TODO DBUserPater: pk: USERID#UID sk: DATA (or sk: GAME_ID#1) AND Service row pk: USERNAME#pechata sk: userId { userId: UID}. If user changing userName we will destroy this and create new one
+    private client: DynamoDBClient = new DynamoDBClient();
+    private documentClient: DynamoDBDocumentClient = DynamoDBDocumentClient.from(this.client);
+    private tableName: string = (() => {
+        if (!process.env.TABLE_NAME) {
+            throw new Error('Cannot get table name');
+        }
+        return process.env.TABLE_NAME;
+    })();
+}
