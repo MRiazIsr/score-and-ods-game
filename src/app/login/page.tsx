c5a76f983478000254a72035167e99a3f21fd19d
Mark Riaz <markriaz13@gmail.com>
2025-07-31 01:09:45 +0300

    Images Fix

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,14 +1,14 @@
 import { getSession } from "@/app/actions/auth";
 import LoginForm from "@/app/login/LoginForm";
-import { redirect } from "next/navigation"
 import {IronSession} from "iron-session";
 import {SessionData} from "@/app/lib/auth/types";
+import {router} from "next/client";
 
 const LoginPage = async () => {
     const session: IronSession<SessionData> = await getSession();
 
     if (session.isLoggedIn) {
-        redirect("/");
+        await router.push("/");
     }
 
     return ( <LoginForm/> );
