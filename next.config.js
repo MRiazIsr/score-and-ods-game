253ec5727286b6816ccd22cd0fbaeadfcf998b86
Mark Riaz <markriaz13@gmail.com>
2025-07-31 23:21:45 +0300

    Improved doker file, improved deploy

--- next.config.js
+++ next.config.js
@@ -1,18 +1,12 @@
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
-
-    experimental: {
-        serverComponentsExternalPackages: [
-            '@aws-sdk/client-dynamodb',
-            '@aws-sdk/lib-dynamodb',
-        ],
-    },
-
     serverExternalPackages: [
         '@aws-sdk/client-dynamodb',
         '@aws-sdk/lib-dynamodb',
     ],
-
+    /* config options here */
     env: {
         DB_TYPE: process.env.DB_TYPE,
         TABLE_NAME: process.env.TABLE_NAME,
@@ -20,22 +14,10 @@ const nextConfig = {
         AWS_REGION: process.env.AWS_REGION,
         AWS_TABLE_NAME: process.env.AWS_TABLE_NAME,
     },
-
     images: {
-        formats: ['image/webp', 'image/avif'],
         remotePatterns: [new URL('https://crests.football-data.org/**')],
     },
 
-    async headers() {
-        return [
-            {
-                source: '/(.*)',
-                headers: [
-                    { key: 'X-Frame-Options', value: 'DENY' },
-                    { key: 'X-Content-Type-Options', value: 'nosniff' },
-                    { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
-                ],
-            },
-        ];
-    },
 };
+
+module.exports = nextConfig;
